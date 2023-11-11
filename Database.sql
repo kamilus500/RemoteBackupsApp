ed8d8e9865cf446fbc3021114acc92abcdb69914
kamilus500 <kkurzeja321@gmail.com>
2023-11-11 11:24:16 +0100

    fix appsettings

--- Database.sql
+++ Database.sql
@@ -14,6 +14,14 @@ AesIv VARBINARY(MAX) NOT NULL,
 IsDeleted BIT NOT NULL
 ) 
 
+CREATE TABLE UserTable(
+Id UNIQUEIDENTIFIER PRIMARY KEY,
+Email NVARCHAR(30) NOT NULL,
+UserName NVARCHAR(30) NOT NULL,
+PasswordHashed VARBINARY(MAX) NOT NULL,
+IsLogin BIT NOT NULL
+)
+
 --create procedure
 CREATE OR ALTER PROCEDURE CreateBackup
     @BackupName NVARCHAR(30),
@@ -30,6 +38,63 @@ BEGIN
     INSERT INTO BackupTable (Id, BackupName, CreationDate, Size, EncryptedData, ContentType, AesKey, AesIv, IsDeleted)
     VALUES (@NewId, @BackupName, @CreationDate, @Size, @EncryptedData, @ContentType, @AesKey, @AesIv, 0);
 END
+
+CREATE OR ALTER PROCEDURE LoginUser
+	@UserName NVARCHAR(30),
+	@Password NVARCHAR(30)
+AS
+BEGIN
+	DECLARE @StoredPasswordHash VARBINARY(MAX);
+	DECLARE @IsLogin BIT;
+
+    SELECT @StoredPasswordHash = PasswordHashed
+    FROM UserTable
+    WHERE UserName = @Username;
+
+	SELECT @IsLogin = IsLogin
+	FROM UserTable
+	WHERE UserName = @UserName;
+
+	IF @IsLogin = 1 
+	BEGIN
+		Print 'Uzytkownik jest juz zalogowany';
+	END
+	ELSE 
+	BEGIN
+		IF @StoredPasswordHash IS NOT NULL
+		BEGIN
+
+			IF @StoredPasswordHash = HASHBYTES('SHA2_512', @Password)
+			BEGIN
+				UPDATE UserTable
+				SET IsLogin = 1
+				WHERE UserName = @UserName;
+
+				SELECT 1 
+			END
+			ELSE
+			BEGIN
+				SELECT 0;
+			END
+		END
+		ELSE
+		BEGIN
+			SELECT -1;
+		END
+	END
+END
+
+CREATE OR ALTER PROCEDURE LogOut 
+	@UserName NVARCHAR(30)
+AS
+BEGIN
+	UPDATE UserTable
+	SET IsLogin = 0
+	WHERE UserName = @UserName
+END
+
+SELECT *
+FROM dbo.UserTable
 
 SELECT *
 FROM dbo.BackupTable
