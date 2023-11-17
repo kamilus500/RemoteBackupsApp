9c7b33158184145c57ac6baf1668499762444747
kamilus500 <kkurzeja321@gmail.com>
2023-11-17 21:02:16 +0100

    add basic unit tests

---
+++ RemoteBackupsApp.UnitTests/Tests/EncryptionTests.cs
@@ -0,0 +1,44 @@
+﻿namespace RemoteBackupsApp.UnitTests.Tests
+{
+    public class EncryptionTests
+    {
+        [Fact]
+        public void Encrypt_ReturnEncryptViewModel()
+        {
+            var bytes = new byte[]
+            {
+                121,
+                09,
+                26,
+                87
+            };
+
+            var encryptionService = new EncryptionService();
+
+            var result = encryptionService.Encrypt(bytes);
+
+            Assert.NotNull(result);
+            Assert.Null(result.BackupName);
+        }
+
+        [Fact]
+        public void Decrypt_ReturnEncryptViewModel()
+        {
+            var bytes = new byte[]
+            {
+                01,
+                05,
+                99,
+                127
+            };
+
+            var encryptionService = new EncryptionService();
+
+            var encryptionViewModel = encryptionService.Encrypt(bytes);
+
+            var result = encryptionService.Decrypt(encryptionViewModel.Content, encryptionViewModel.AesKey, encryptionViewModel.AesIv);
+
+            Assert.NotNull(result);
+        }
+    }
+}
