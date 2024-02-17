8e1c6b2d77b26f6f0420443fa2492c3e92a9de81
kamilus500 <kkurzeja321@gmail.com>
2024-02-17 15:47:23 +0100

    add hidden file

---
+++ RemoteBackupsApp.Domain/ViewModels/Backup/FileProcessViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RemoteBackupsApp.Domain.ViewModels.Backup
+{
+    public class FileProcessViewModel
+    {
+        public byte[] Content { get; set; }
+        public string FileName { get; set; }
+        public string UserId { get; set; }
+        public string ContentType { get; set; }
+        public int FileLength { get; set; }
+    }
+}
